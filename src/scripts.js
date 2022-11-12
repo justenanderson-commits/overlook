d2db2d647c9bc8e3094d6ffd0ce200b767e2a0fb
Justen Anderson <justenanderson@gmail.com>
2022-11-11 19:27:29 -0700

    Populate previous and upcoming stays from the Customer class.

--- src/scripts.js
+++ src/scripts.js
@@ -13,38 +13,64 @@ let allCustomersData, singleCustomerData, allBookingsData, allRoomsData, custome
 // Query Selectors
 const customerWelcome = document.getElementById('text--customer-message')
 
+const upcomingStaysTable = document.getElementById('table--upcoming-stays-body')
+const previousStaysTable = document.getElementById('table--previous-stays-body')
+
 
 // Promises - REMOVE HARD CODING WHEN THESE ARE WORKING:
 
 // UPDATE: The solution is to instantiate a new class within the promise.all. I just gotta figure out how to do it.
-const onLoadPromises = () => { 
+const onLoadPromises = () => {
   Promise.all([getSingleCustomer(48), getAllBookings(), getAllRooms()])
-  .then(data => {
-    // singleCustomerData should now be hard-coded for Kaylee Herman. Remove the (48) and pass in the userID to make it dynamic.
-    singleCustomerData = data[0]
-    // console.log('Customer Data: ', singlesingleCustomerData)
-    
-    allBookingsData = data[1]
-    // console.log('All bookings data: ', allBookingsData)
-    let customerBookings = allBookingsData.bookings.filter(booking => booking.userID === 48)
-    // console.log('Customer Bookings: ', customerBookings)
-    
-    customer = new Customer(singleCustomerData)
-    // console.log('New Customer Object: ', customer)
-    customer.getNewBookings(customerBookings)
-    customer.getOldBookings(customerBookings)
+    .then(data => {
+      // singleCustomerData should now be hard-coded for Kaylee Herman. Remove the (48) and pass in the userID to make it dynamic.
+      singleCustomerData = data[0]
+      // console.log('Customer Data: ', singlesingleCustomerData)
 
-    console.log('Customer newBookings property: ', customer.newBookings)
-    console.log('Customer oldBookings property: ', customer.oldBookings)
-    
+      allBookingsData = data[1]
+      // console.log('All bookings data: ', allBookingsData)
+      let customerBookings = allBookingsData.bookings.filter(booking => booking.userID === 48)
+      // console.log('Customer Bookings: ', customerBookings)
 
-    allRoomsData = data[2]
-    // console.log('All rooms data: ', allRoomsData)
-    allRoomsData.rooms.forEach(room => {
-      room = new Room(room)
-      loadCustomer()
+      customer = new Customer(singleCustomerData)
+      // console.log('New Customer Object: ', customer)
+      customer.getNewBookings(customerBookings)
+      customer.getOldBookings(customerBookings)
+      customer.newBookings.forEach(booking => {
+        upcomingStaysTable.innerHTML += `<tr>
+        <td>${booking.date}</td>
+        <td>Add room</td>
+        <td>${booking.id}</td>
+        <td>Add price</td>
+        </tr>`
+      })
+      customer.oldBookings.forEach(booking => {
+        previousStaysTable.innerHTML += `<tr>
+        <td>${booking.date}</td>
+        <td>Add room</td>
+        <td>${booking.id}</td>
+        <td>Add price</td>
+        </tr>`
+      })
+
+
+
+      // // Finish this to add the table contents
+      // upcomingStaysTable.innerHTML = 
+
+
+
+      console.log('Customer newBookings property: ', customer.newBookings)
+      console.log('Customer oldBookings property: ', customer.oldBookings)
+
+
+      allRoomsData = data[2]
+      // console.log('All rooms data: ', allRoomsData)
+      allRoomsData.rooms.forEach(room => {
+        room = new Room(room)
+        loadCustomer()
+      })
     })
-  })
 }
 
 // Event Listeners
---
+++ test/Customer-test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import Customer from '../src/Customer';
+
+
+// describe('Customer', () = {
+//   let testCustomer;
+//   beforeEach(() => {
+//     testCustomer = new Customer(customerData)
+
+//   })
+// })
